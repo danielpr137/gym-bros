165f88ef13c4d2ac4cc8847cd4893d4d89fc951f
Ofir Imas <ofir.imas@gloat.com>
2023-08-11 12:56:35 +0300

    init with docker

--- auth/main.py
+++ auth/main.py
@@ -1,8 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello from Microservice 1"}
+@app.route("/login")
+def login(request: str = Header(None)):
+    auth = request.authorization
+    if not auth:
+        return "missing credentials", 401
+
+    return createJWT(auth.username, "fake_key")
+
+
+@app.route("/validate", methods=["POST"])
+def validate():
+    encoded_jwt = request.headers["Authorization"]
+
+    if not encoded_jwt:
+        return "missing credentials", 401
+
+    encoded_jwt = encoded_jwt.split(" ")[1]
+
+    try:
+        decoded = jwt.decode(
+            encoded_jwt, "fake_key", algorithms=["HS256"]
+        )
+    except:
+        return "not authorized", 403
+
+    return decoded, 200
+
+
+def createJWT(username, secret):
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc)
+            + datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz,
+        },
+        secret,
+        algorithm="HS256",
+    )
+    
 
